863c41af64666a4f3c2f5f8e05942c11070c0e16
m-jurzyk <jurzyk.maciej@gmail.com>
2022-12-02 11:44:39 +0100

    Stworzenie skryptu startup, a potem wykrozystanie komendy source

---
+++ Proba Source.R
@@ -0,0 +1,5 @@
+# Odpalamy Startup.R komendą source, jeśli nie ma jej w bibliotece trzeba będzie 
+#dodać parę rzeczy do niej potem ale to może easy- kto wie 
+
+
+source("Startup.R")
---
+++ Startup.R
@@ -0,0 +1,12 @@
+# Zaczniemy dziś ogarniać startupowy skrytp, żeby uruchomil kilka bibliotek 
+#od razu po włączeniu
+
+library(tidyverse)
+library(ggplot2)
+library(gganimate)
+library(dplyr)
+library(patchwork)
+library(RColorBrewer)
+
+# powinno śmigać ale to dopiero pierwsza próba - zobaczmy - zapisuje plik i próbuje 
+# otworzyć w nowym skrypcie 
